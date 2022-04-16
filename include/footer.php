20c17d4758145b0aeaec0ed658de837cb34f1037
Vishal Mule <99642066+vmule1711@users.noreply.github.com>
2022-04-16 20:37:32 +0530

    include header footer php

---
+++ include/footer.php
@@ -0,0 +1,79 @@
+<footer>
+        <div class="eshocan_footer_links_block">
+            <div  data-aos="fade-down" data-aos-duration="1000">
+                <a href="index.php"><img src="img/Newlogo.png" class="eshocan_brand_logo" alt="eShocan" title="eShocan"></a>
+                <p class="eshocan_footer_copy">eShocan is a full-scale engineering services company, mobilizing our industry experience and global network to deliver products from concept to production for the automotive industry and beyond.</p>   
+                <div class="eshocan_social_media desktop-block" >
+                    <ul>
+                        <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fab fa-twitter-square"></i></a></li>
+                        <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+    
+            <div data-aos="fade-down" data-aos-duration="1200">
+                <h2><a href="">Our Services</a></h2>
+                <ul>
+                    <li><a href="services.php">Technology</a></li>
+                    <li><a href="engineering.php">Engineering</a></li>
+                    <li><a href="ProductLifeCycleManagement.php">PLM</a></li>
+                    <li><a href="Industry4.php">Digital 4.0</a></li>
+                    <li><a href="consulting-and-staffing.php">Consulting & Staffing</a></li>
+                    <li><a href="Teams.php">Teams</a></li>
+                </ul>
+            </div>
+    
+            <div  data-aos="fade-down" data-aos-duration="1300">
+                <h2><a href="">Quick Links</a></h2>
+                <ul>
+                    <li><a href="Expertise.php">Platform development</a></li>
+                    <li><a href="Expertise_HMI_Design.php">HMI design</a></li>
+                    <li><a href="Expertise_Autonomous_driving.php">ADAS Software dev &<br>Validation</a></li>
+                    <li><a href="Expertise_Mobile_application_development.php">Mobile application<br>developement</a></li>
+                    <li><a href="Expertise_Product_developement.php">Prodcut Design</a></li>
+                    <li><a href="Virtual Simulation.php">Virtual simulation</a></li>
+                </ul>
+            </div>
+    
+            <div  data-aos="fade-down" data-aos-duration="1400">
+                <h3>Contact Us</h3>
+                <ul class="contact-location">
+                    <li><img src="img/pin.png" alt=""> <a href="">USA (Ypsilanti, Michigan)</a></li>
+                    <li><img src="img/pin.png" alt=""> <a href="">India (Bangalore, KA)</a></li>
+                </ul>
+                <ul class="contact-details">
+                    <li><span><i class="fa fa-phone" aria-hidden="true"></i></span> <a href="">+1 (734)544-1598</a></li>
+                    <li><span><i class="fa fa-envelope" aria-hidden="true"></i></span> <a href="">contactus@eshocan.com</a></li>
+                </ul>
+            </div>
+        </div><!--eshocan_footer_links_block-->
+
+        <div class="eshocan_social_media mobile-block" >
+            <ul>
+                <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter-square"></i></a></li>
+                <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="copyright-design-develope">
+            <p class="eshocan_copy_write">Copyright 2021 all rights reserved</p>
+            <p class="eshocan_copy_write">Design and Developed by <a href="http://www.southernlightds.com/">Southern Light Design Studio</a></p>
+        </div>
+        
+    </footer>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
+    <script src="https://eshocan.com/wp-content/themes/eshocan/js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://kit.fontawesome.com/c8d133034e.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="js/main.js"></script>
+
+    <script>
+        $(function() {
+            AOS.init();
+        });
+    </script>
